eb63049a393135d8de77e29bf0cd618484f9794b
Dev Raja <Dev@gmail.com>
2026-06-29 22:02:14 +0530

    sum program for edu

---
+++ src/.idea/sum.java
@@ -0,0 +1,12 @@
+
+
+public class sum {
+    public static void main(String[] args) {
+        int arr[]={2,16,3,45,67,12,23,88};
+        int sum=0;
+        for(int i=0;i< arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+    }
+}
